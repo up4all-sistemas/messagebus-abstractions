db897c08ad8485c70a91a421bb505ecd20b485ea
up4all-sistemas <artur@up4all.com.br>
2021-05-03 13:17:33 -0300

    ajustes

--- Up4All.Framework.MessageBus.Abstractions/Configurations/MessageBusConfiguration.cs
+++ Up4All.Framework.MessageBus.Abstractions/Configurations/MessageBusConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using Up4All.Framework.MessageBus.Abstractions.Interfaces;
 using Up4All.Framework.MessageBus.Abstractions.Mocks;
 using Up4All.Framework.MessageBus.Abstractions.Options;
 
@@ -16,34 +17,34 @@ namespace Up4All.Framework.MessageBus.Abstractions.Configurations
         public static void AddMessageBusQueueClient<T>(this IServiceCollection services, IConfiguration configuration) where T : MessageBusQueueClient
         {
             services.AddConfigurationBinder(configuration);
-            services.AddScoped<MessageBusQueueClient, T>();
+            services.AddScoped<IMessageBusQueueClient, T>();
         }
 
         public static void AddMessageBusTopicClient<T>(this IServiceCollection services, IConfiguration configuration) where T : MessageBusTopicClient
         {
             services.AddConfigurationBinder(configuration);
-            services.AddScoped<MessageBusTopicClient, T>();
+            services.AddScoped<IMessageBusPublisher, T>();
         }
 
         public static void AddMessageBusSubscribeClient<T>(this IServiceCollection services, IConfiguration configuration) where T : MessageBusSubscribeClient
         {
             services.AddConfigurationBinder(configuration);
-            services.AddScoped<MessageBusSubscribeClient, T>();
+            services.AddScoped<IMessageBusConsumer, T>();
         }
 
         public static void AddMessageBusQueueClientMocked<T>(this IServiceCollection services) where T : MessageBusQueueClientMock
         {            
-            services.AddScoped<MessageBusQueueClientMock, T>();
+            services.AddScoped<IMessageBusQueueClient, T>();
         }
 
         public static void AddMessageBusTopicClientMocked<T>(this IServiceCollection services) where T : MessageBusTopicClientMock
         {            
-            services.AddScoped<MessageBusTopicClientMock, T>();
+            services.AddScoped<IMessageBusPublisher, T>();
         }
 
         public static void AddMessageBusSubscribeClientMocked<T>(this IServiceCollection services) where T : MessageBusSubscribeClientMock
         {            
-            services.AddScoped<MessageBusSubscribeClientMock, T>();
+            services.AddScoped<IMessageBusConsumer, T>();
         }
     }
 }
